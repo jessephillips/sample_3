10638cba3a734531b0fa6ab18f59755db3f5e693
Jesse Phillips <mr.jesse.phillips@gmail.com>
2011-06-11 12:08:21 -0400

    Added password and encrypted password attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe User do
 
 before(:each) do
-	@attr = { :name => "Test Name", :email => "email@email.com" }
+	@attr = {   :name => "Test Name", 
+				:email => "email@email.com", 
+				:password => "foobar",
+				:password_confirmation => "foobar"
+				}
 end
 
   it "should create a new instance given a valid attribute" do
@@ -53,6 +57,45 @@ end
   	User.create!(@attr.merge(:email => upcased_email))
   	user_with_duplicate_email = User.new(@attr)
   	user_with_duplicate_email.should_not be_valid
+  end
+  
+  describe "passwords" do
+  
+  	before(:each) do
+  		@user = User.new(@attr)
+  	end
+  
+  	it "should have a password attribute" do
+  		@user.should respond_to(:password)
+  	end
+  	
+  	it "should have a password confirmation attribute" do
+  		@user.should respond_to(:password_confirmation)
+  	end
+  	
+  end
+  
+  describe "password validations" do
+  	it "should require a password" do
+  		User.new(@attr.merge(:password => "", :password_confirmation => "")).
+  		should_not be_valid
+  	end
+  	
+  	it "should require a matching password confirmation" do
+  		User.new(@attr.merge(:password_confirmation => "invalid")).
+  		should_not be_valid
+  	end
+  end
+  
+  describe "password encryption" do
+  	
+  	before(:each) do
+  		@user = User.create!(@attr)
+  	end
+  	
+  	it "should have an encrypted password attribute" do
+  		@user.should respond_to(:encrypted_password)
+  	end
   end
   
 end
